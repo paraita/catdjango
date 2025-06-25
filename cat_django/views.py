9d83368732e72e562e91c255811b61581412c635
Paraita Wohler <paraita.wohler@gmail.com>
2025-06-24 16:57:30 -1000

    Fix settings module path and string comparison bug

--- cat_django/views.py
+++ cat_django/views.py
@@ -9,7 +9,7 @@ from cat_django.models import Enigme
 
 def enigme(request):
     _id = request.GET.get("id", "-1")
-    if _id is "-1":
+    if _id == "-1":
         return HttpResponse("pas bon")
     else:
         e = Enigme.objects.get(id_enigme=_id)
